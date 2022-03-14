a7afc6668ae8a31ae18f7700ae359889d90480bc
WIN10 <jwoan_395@hotmail.com>
2022-03-14 17:05:02 +0200

    Practice Project V2 push

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, ChangeEvent ,useState} from "react";
 import './App.css';
+import { IGame } from "./interfaces";
 
-function App() {
-  return (
+const App = () => {
+
+  const [game, setGame] = useState<string>("")
+  const [company, setCompany] = useState<string>("")
+  const [Year, setYear] = useState<number>(0)
+  const [GameAdd, setGameAdd] = useState<IGame[]>([]);
+
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if(event.target.name === "GameName"){
+      setGame(event.target.value)
+    }else if (event.target.name ==="Company"){
+      setCompany(event.target.value)
+    } else {
+      setYear(Number(event.target.value));
+    }
+  };
+
+  const addGame = (): void => {
+    // We take the interfaces name and match it with the variables we created
+    const newGame = {GameName: game, CompanyName: company, YearOfRelease: Year};
+
+    setGameAdd([...GameAdd, newGame])
+  }
+
+
+
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input type='text' placeholder="GameName" name="Game" onChange={handleChange}/>
+          <input type='text' placeholder='Company' name="Company" onChange={handleChange}/>
+          <input type='number' name="Year" placeholder='Year Of Release' onChange={handleChange}/>
+        </div>
+        <button onClick={addGame}>Add Game</button>
+      </div>
+      <div className="AddGame"></div>
     </div>
   );
 }
---
+++ src/interfaces.ts
@@ -0,0 +1,5 @@
+export interface IGame {
+    GameName : string;
+    CompanyName: string;
+    YearOfRelease: number;
+}
